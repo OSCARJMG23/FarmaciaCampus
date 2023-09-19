d7627946b2df5e6dbc78550f49aa96b032725ed5
KevinRinc0n <rinconkdavid@gmail.com>
2023-09-19 14:23:28 -0500

    On branch Kevin

--- Persistencia/Data/ApiFarmaciaContext.cs
+++ Persistencia/Data/ApiFarmaciaContext.cs
@@ -10,24 +10,21 @@ namespace Persistencia.Data;
 
 public class ApiFarmaciaContext : DbContext
 {
-    public class ApiFarmaciaContext : DbContext
+    public ApiFarmaciaContext(DbContextOptions options) : base(options)
     {
-        public ApiFarmaciaContext(DbContextOptions options) : base(options)
-        {
-        }
-        public DbSet<Paciente> Pacientes { get; set; }
-        public DbSet<Empleado> Empleados { get; set; }
-        public DbSet<Proveedor> Proveedores { get; set; }
-        public DbSet<Compra> Compras { get; set; }
-        public DbSet<Medicamento> Medicamentos { get; set; }
-        public DbSet<MedicamentoComprado> MedicamentosComprados { get; set; }
-        public DbSet<MedicamentoVendido> MedicamentosVendidos { get; set; }
-        public DbSet<Venta> Ventas { get; set; }
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
+    }
+    public DbSet<Paciente> Pacientes { get; set; }
+    public DbSet<Empleado> Empleados { get; set; }
+    public DbSet<Proveedor> Proveedores { get; set; }
+    public DbSet<Compra> Compras { get; set; }
+    public DbSet<Medicamento> Medicamentos { get; set; }
+    public DbSet<MedicamentoComprado> MedicamentosComprados { get; set; }
+    public DbSet<MedicamentoVendido> MedicamentosVendidos { get; set; }
+    public DbSet<Venta> Ventas { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
 
-            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
-        }
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
     }
 }
