dac0bca9834cf3066115f2a9832f2c73531ec581
OSCARJMG23 <oscarjulianm2112@gmail.com>
2023-09-19 14:12:29 -0500

    feat: :sparkles: Add DbSet
    AddDbSet in dbcontext

--- Persistencia/Data/ApiFarmaciaContext.cs
+++ Persistencia/Data/ApiFarmaciaContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
+using Dominio.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Persistencia.Data
@@ -12,6 +13,14 @@ namespace Persistencia.Data
         public ApiFarmaciaContext(DbContextOptions options) : base(options)
         {
         }
+        public DbSet<Paciente> Pacientes { get; set; }
+        public DbSet<Empleado> Empleados { get; set; }
+        public DbSet<Proveedor> Proveedores { get; set; }
+        public DbSet<Compra> Compras { get; set; }
+        public DbSet<Medicamento> Medicamentos { get; set; }
+        public DbSet<MedicamentoComprado> MedicamentosComprados { get; set; }
+        public DbSet<MedicamentoVendido> MedicamentosVendidos { get; set; }
+        public DbSet<Venta> Ventas { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
