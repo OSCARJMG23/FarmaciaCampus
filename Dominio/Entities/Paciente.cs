ab820f6681e83b5872dd095ea90c976925da1c67
OSCARJMG23 <oscarjulianm2112@gmail.com>
2023-09-27 12:44:10 -0500

    feat: :fire: remove collection

--- Dominio/Entities/Paciente.cs
+++ Dominio/Entities/Paciente.cs
@@ -3,10 +3,9 @@ namespace Dominio.Entities;
 public class Paciente : BaseEntity
 {
     public string Nombre { get; set; }
-    public string IdDireccionFk { get; set; }
+    public int IdDireccionFk { get; set; }
     public Direccion Direccion { get; set; }
     public string Telefono { get; set; }
     public ICollection<RecetaMedica> Recetas { get; set; }
     public ICollection<MovimientoInventario> MovimientosInventario { get; set; }
-    public ICollection<RefreshToken> RefreshTokens { get; set; } = new HashSet<RefreshToken>();
 }
